99e656977fdfb1783e218e1054b90052cadbacf1
iamnotstatic <staticdev20046@gmail.com>
2024-04-16 18:59:47 +0700

    Add HealthController and HealthModule to project

---
+++ src/modules/health/health.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Health')
+@Controller('health')
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly database: TypeOrmHealthIndicator,
+  ) {}
+
+  @ApiOperation({ summary: 'Application Health check' })
+  @Get()
+  @HealthCheck()
+  check(): Promise<HealthCheckResult> {
+    return this.health.check([() => this.database.pingCheck('database')]);
+  }
+}

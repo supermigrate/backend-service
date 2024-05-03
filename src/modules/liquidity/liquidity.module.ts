6a6e4e45e338bf69c2721c8743160221dbd2f868
iamnotstatic <staticdev20046@gmail.com>
2024-05-03 19:59:58 +0700

    Add TypeOrmModule and JwtModule imports in LiquidityModule

--- src/modules/liquidity/liquidity.module.ts
+++ src/modules/liquidity/liquidity.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LiquidityService } from './liquidity.service';
 import { LiquidityController } from './liquidity.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Liquidity } from './entities/liquidity.entity';
+import { User } from '../user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Liquidity, User]), JwtModule],
   providers: [LiquidityService],
   controllers: [LiquidityController],
   exports: [LiquidityService],

626f0c9482022839c827c7616835bd004410b922
iamnotstatic <staticdev20046@gmail.com>
2024-06-28 01:47:46 +0700

    Add caching for Ethereum price in SharedService and LaunchboxService

--- src/common/helpers/shared/shared.service.ts
+++ src/common/helpers/shared/shared.service.ts
@@ -4,9 +4,29 @@ import { env } from '../../config/env';
 
 @Injectable()
 export class SharedService {
+  private readonly cache: { ethPrice: number; timestamp: number } = {
+    ethPrice: 0,
+    timestamp: 0,
+  };
+
   private readonly Logger = new Logger(SharedService.name);
 
   async getEthPriceInUsd(): Promise<number> {
+    const CACHE_DURATION = 5 * 60 * 1000; // Cache duration in milliseconds (5 minutes)
+    const currentTime = Date.now();
+
+    if (this.cache && currentTime - this.cache.timestamp < CACHE_DURATION) {
+      return this.cache.ethPrice;
+    }
+
+    const ethPrice = await this.fetchEthPrice();
+    this.cache.ethPrice = ethPrice;
+    this.cache.timestamp = currentTime;
+
+    return ethPrice;
+  }
+
+  private async fetchEthPrice(): Promise<number> {
     try {
       const browser = await chromium.launch({ headless: true });
       const page = await browser.newPage();
